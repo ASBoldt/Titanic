61fdc619b53e3be904018de23400425283b4cade
ASBoldt <as.boldt@gmail.com>
2014-02-28 11:25:11 -0300

    Novo.

--- Funcoes.R
+++ Funcoes.R
@@ -7,3 +7,7 @@ prop.table() # para obter as proporcoes
 # No arquivo .gitignore coloquei: data, logs e cache
 # Fiz isso para nao compartilhar essas pastas, ou seja,
 # para nao expor os dados e os resultados.
+
+# Outras informacoes para o github: Atualizar modificacoes
+# Na aba Git > More > Shell, escrever:
+# git push -u origin master
