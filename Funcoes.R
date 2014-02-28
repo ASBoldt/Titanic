48ecbeef711dc54e4265ecdaa40a0f2767d7189e
ASBoldt <as.boldt@gmail.com>
2014-02-28 10:48:43 -0300

    Test

---
+++ Funcoes.R
@@ -0,0 +1,9 @@
+# Resumo de funcoes
+
+table() # para sumarizar
+prop.table() # para obter as proporcoes
+
+# No git
+# No arquivo .gitignore coloquei: data, logs e cache
+# Fiz isso para nao compartilhar essas pastas, ou seja,
+# para nao expor os dados e os resultados.
